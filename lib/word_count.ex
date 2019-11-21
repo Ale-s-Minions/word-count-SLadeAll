9ea2a6407b73afd4f1f1e205090f90cab64b7877
oscar <oscarjavier_vera@ucol.mx>
2019-11-21 09:19:03 -0600

    Hasta donde pude

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -4,7 +4,16 @@ defmodule WordCount do
 
   Words are compared case-insensitively.
   """
-  @spec count(String.t()) :: map
+  @spec count(String.t) :: map
   def count(sentence) do
+    sentence
+    |>  String.downcase()
+    |>  String.replace(~r/@|#|:|_|-|\$|!|\^|&||%|,/u, " ")
+    |>  String.split()
+    |>  Enum.reduce(%{},fn word, counts ->
+      counts
+      |> Map.put_new(word, 0)
+      |> Map.update(word, 1,&(&1 + 1))
+    end)
   end
 end
